7ba0b16887935453c92998a588b3885421afa180
ngseke <ngseke@gmail.com>
2024-03-02 18:53:09 +0800

    chore setup ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:@next/next/recommended',
+    'next/core-web-vitals',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react',
+  ],
+  rules: {
+    '@typescript-eslint/comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+      generics: 'ignore',
+    }],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    'react/jsx-indent': ['warn', 2, {
+      checkAttributes: true,
+      indentLogicalExpressions: true,
+    }],
+    'react/prop-types': ['off'],
+    'react/jsx-indent-props': ['warn', 2],
+    'react/jsx-closing-bracket-location': ['warn', 'tag-aligned'],
+    'jsx-quotes': ['warn', 'prefer-double'],
+    'react/jsx-curly-spacing': ['warn', { when: 'never', children: true }],
+    'react/jsx-tag-spacing': ['warn', {
+      closingSlash: 'never',
+      beforeSelfClosing: 'always',
+      afterOpening: 'never',
+      beforeClosing: 'never',
+    }],
+    'react/self-closing-comp': 'warn',
+    'react/jsx-fragments': 'warn',
+    'react/jsx-equals-spacing': [2, 'never'],
+  },
+}
