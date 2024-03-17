5d0e4a4f5345e2c62d3e044c809fca1e5d566ba1
ngseke <ngseke@gmail.com>
2024-03-17 12:11:33 +0800

    chore: update ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -42,6 +42,7 @@ module.exports = {
     }],
     '@typescript-eslint/explicit-function-return-type': ['off'],
     '@typescript-eslint/no-misused-promises': ['off'],
+    '@typescript-eslint/strict-boolean-expressions': ['off'],
     'react/jsx-indent': ['warn', 2, {
       checkAttributes: true,
       indentLogicalExpressions: true,
